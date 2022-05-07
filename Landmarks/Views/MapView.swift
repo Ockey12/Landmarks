5f47b06f9d1be4d24c6f3b138741ce029faad02e
OnagaHaruki <haloupgarou@gmail.com>
2022-05-07 16:44:49 +0900

    SwiftUI Essentials - Building Lists and Navigation

---
+++ Landmarks/Views/MapView.swift
@@ -0,0 +1,35 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by オナガ・ハルキ on 2022/05/06.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var coordinate: CLLocationCoordinate2D
+    
+    @State private var region = MKCoordinateRegion()
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+            .onAppear {
+                setRegion(coordinate)
+            }
+    }
+    
+    private func setRegion(_ coordinate: CLLocationCoordinate2D) {
+        region = MKCoordinateRegion(
+            center: coordinate,
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView(coordinate: CLLocationCoordinate2D(latitude: 34.001_286, longitude: -116.166_868))
+    }
+}
+
